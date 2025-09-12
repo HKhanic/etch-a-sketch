5a4c9befe4f67c071cd7d4d3d684f522b3502800
Hakan <hakancopuroglu@gmail.com>
2025-09-12 21:04:17 +0200

    change the sketchboard to be flexible up to 100x100 cells

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const container = document.querySelector("#container");
+const resize = document.querySelector("#resize");
 
-const CONTAINER_ROWS = 16;
-const CONTAINER_COLUMNS = 16;
-
-for (let index = 0; index < CONTAINER_ROWS; index++) {
-    
-    for (let index = 0; index < CONTAINER_COLUMNS; index++) {
-        const div = document.createElement("div");
-        container.appendChild(div);
-    }
-    
-}
+drawBoard(16);
 
 container.addEventListener("mouseover", e => {
-    e.target.style.backgroundColor = "black";
+    e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, 
+                                        ${Math.floor(Math.random() * 256)}, 
+                                        ${Math.floor(Math.random() * 256)})`;
 });
+
+resize.addEventListener("click", e => {
+    let validInput = false;
+    let inputSize;
+
+    while(!validInput) {
+        inputSize = Number.parseInt(prompt("Input size:"));
+
+        inputSize <= 100 ? validInput = true : validInput = false;
+    }
+
+    drawBoard(inputSize);
+});
+
+function drawBoard(size) {
+
+    container.replaceChildren();
+
+    for (let index = 0; index < size; index++) {
+        
+        for (let index = 0; index < size; index++) {
+            const div = document.createElement("div");
+            div.style.flex = `0 0 calc(100% / ${size})`;
+            container.appendChild(div);
+        }
+        
+    }
+}
