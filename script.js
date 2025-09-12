0e1c362cd19196f1de2b7e3b733e57c99fbc540e
Hakan <hakancopuroglu@gmail.com>
2025-09-12 19:22:40 +0200

    create flexbox grid

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for (let index = 0; index < CONTAINER_ROWS; index++) {
     for (let index = 0; index < CONTAINER_COLUMNS; index++) {
         const div = document.createElement("div");
         div.style.border = "1px solid black";
-        div.textContent = "adwdwwd";
+        //div.textContent = "adwdwwd";
         container.appendChild(div);
     }
     
