3793649bcb55e19e5c23e910272f4b9ccf77b022
Hakan <hakancopuroglu@gmail.com>
2025-09-12 19:37:48 +0200

    added mouseover event

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ for (let index = 0; index < CONTAINER_ROWS; index++) {
     
     for (let index = 0; index < CONTAINER_COLUMNS; index++) {
         const div = document.createElement("div");
-        div.style.border = "1px solid black";
-        //div.textContent = "adwdwwd";
         container.appendChild(div);
     }
     
 }
+
+container.addEventListener("mouseover", e => {
+    e.target.style.backgroundColor = "black";
+});
