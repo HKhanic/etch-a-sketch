00edc0e1b817e737d49e2b580b563fee3a009c88
Hakan <hakancopuroglu@gmail.com>
2025-09-13 10:45:16 +0200

    add fading opacity

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ container.addEventListener("mouseover", e => {
     e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, 
                                         ${Math.floor(Math.random() * 256)}, 
                                         ${Math.floor(Math.random() * 256)})`;
+
+    if(parseFloat(e.target.style.opacity) === 0) {
+        return;
+    }
+    let current = parseFloat(e.target.style.opacity) || 1; // start at 1 if not set
+    if (current >= 0) {
+        e.target.style.opacity = (current - 0.1).toFixed(2); // decrease step
+    }
 });
 
 resize.addEventListener("click", e => {
@@ -31,6 +39,7 @@ function drawBoard(size) {
         for (let index = 0; index < size; index++) {
             const div = document.createElement("div");
             div.style.flex = `0 0 calc(100% / ${size})`;
+            div.style.backgroundColor = "white";
             container.appendChild(div);
         }
         
